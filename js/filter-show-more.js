fa9aa9aa7d19df87ac5b9865e5dda63ad2d2ec33
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-13 14:22:58 +0500

    work btns filter

--- js/filter-show-more.js
+++ js/filter-show-more.js
@@ -10,7 +10,9 @@ if (filterForm) {
     })
 
     const showMoreFilterBtn = document.querySelector('.filter-show-more__btn');
-    // console.log(showMoreFilterBtn);
+    const hideFilterBtn = document.querySelector('.hide-filter__btn');
+    const filterOverAutoBtn = document.querySelector('.filter-over-auto__btn');
+    console.log(hideFilterBtn);
 
     // console.log(filterForm);
     const filterHiddenSections = filterForm.querySelectorAll('.filter-hidden');
@@ -20,8 +22,32 @@ if (filterForm) {
 
         filterHiddenSections.forEach(filterHiddenSection => filterHiddenSection.classList.remove('none'));
         showMoreFilterBtn.classList.add('none');
+
+        hideFilterBtn.classList.remove('none');
+        filterOverAutoBtn.classList.remove('none');
+        filterScrollElem.classList.remove('filter-over-auto-scroll');
+
     })
 
+    hideFilterBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        filterHiddenSections.forEach(filterHiddenSection => filterHiddenSection.classList.add('none'));
+
+        showMoreFilterBtn.classList.remove('none');
+        hideFilterBtn.classList.add('none');
+        filterOverAutoBtn.classList.add('none');
+    })
+
+
+    const filterScrollElem = document.querySelector('.filter-scroll');
+    console.log(filterOverAutoBtn, filterScrollElem);
+
+    filterOverAutoBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        filterScrollElem.classList.add('filter-over-auto-scroll');
+        filterOverAutoBtn.classList.add('none');
+    })
 
 // const filtersRangeOne = document.querySelectorAll('.filter-show-more__range');
 // const filtersRangeTwo = document.querySelectorAll('.filters-range-two');
