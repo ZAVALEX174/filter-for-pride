cc0baa0e6257dd11811869fab52d25e207af7f78
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-15 13:27:26 +0500

    15-01-2025/13-27

--- js/filter-show-more.js
+++ js/filter-show-more.js
@@ -55,12 +55,11 @@ if (filterForm) {
         return newListItem;
     };
 
-
     if (horLabels) {
         horLabels.forEach(label => {
             const checks = label.querySelector('.checkbox-real');
             checks.checked = false;
-            console.log(checks.checked);
+            // console.log(checks.checked);
             if (checks) {
                 label.addEventListener('change', (e) => {
                     let val = label.querySelector('.option__radio-title');
@@ -90,4 +89,143 @@ if (filterForm) {
             }
         });
     }
+
+    // Получение данных из слайдов от... до....
+    const rangeSlider1 = document.getElementById('range-slider1');
+    const rangeSlider2 = document.getElementById('range-slider2');
+    // const horLabels = horizontalInputs.querySelectorAll('.option__btn');
+
+    if (rangeSlider1) {
+        noUiSlider.create(rangeSlider1, {
+            start: [1, 2000],
+            step: 1,
+            connect: true,
+            keyboardSupport: false,
+            range: {
+                'min': 1,
+                'max': 2000
+            }
+        });
+
+        const input0 = document.getElementById('input-0');
+        const input1 = document.getElementById('input-1');
+        const inputs = [input0, input1];
+
+        rangeSlider1.noUiSlider.on('update', function (values, handle) {
+
+            inputs[handle].value = Math.round(values[handle]);
+        });
+
+        rangeSlider1.noUiSlider.on('keydown', function (values, handle) {
+
+            inputs[handle].value = Math.round(values[handle]);
+        });
+
+        const setRangeSlider1 = (i, value) => {
+
+            let arr = [null, null];
+            arr[i] = value;
+
+            console.log(arr[i]);
+
+            rangeSlider1.noUiSlider.set(arr);
+        };
+
+        inputs.forEach((el, index) => {
+            el.addEventListener('change', (e) => {
+                e.preventDefault();
+                console.log(index);
+                setRangeSlider1(index, e.currentTarget.value);
+            });
+        });
+
+        const getResultFromInputFirstField = document.getElementById('r2');
+        getResultFromInputFirstField.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(inputs[1].value);
+            createElementSliderOne(inputs[0].value, inputs[1].value);
+        })
+    }
+
+    const createElementSliderOne = (fromCount, toCount) => {
+        console.log(toCount);
+        let newListItem = document.createElement('li');
+        newListItem.textContent = `Глубина бурения (max), м: от ${fromCount} до ${toCount}`;
+        newListItem.classList.add('filter__show-content-item');
+
+        let divEl = document.createElement('div');
+        divEl.classList.add('filter__show-content-item__btn-svg');
+        newListItem.appendChild(divEl);
+
+        filterShowContentList.appendChild(newListItem);
+        return newListItem;
+    };
+
+    if (rangeSlider2) {
+        noUiSlider.create(rangeSlider2, {
+            start: [45, 1000],
+            step: 1,
+            connect: true,
+            range: {
+                'min': 45,
+                'max': 1000
+            }
+        });
+
+        const input2 = document.getElementById('input-2');
+        const input3 = document.getElementById('input-3');
+        const inputs = [input2, input3];
+
+
+        rangeSlider2.noUiSlider.on('update', function (values, handle) {
+
+            inputs[handle].value = Math.round(values[handle]);
+        });
+
+        const setRangeSlider2 = (i, value) => {
+
+            let arr = [null, null];
+            arr[i] = value;
+
+            console.log(arr[0], arr[1]);
+
+            rangeSlider2.noUiSlider.set(arr);
+            console.log(arr)
+        };
+
+        inputs.forEach((el, index) => {
+            el.addEventListener('change', (e) => {
+                e.preventDefault();
+                setRangeSlider2(index, e.currentTarget.value);
+            });
+        });
+        const getResultFromInputSecondField = document.getElementById('r2');
+        getResultFromInputSecondField.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(inputs[1].value);
+            createElementSliderTwo(inputs[0].value, inputs[1].value);
+        })
+    }
+
+    const createElementSliderTwo = (fromCount, toCount) => {
+        console.log(toCount);
+        let newListItem = document.createElement('li');
+        newListItem.textContent = `Глубина бурения (max), м: от ${fromCount} до ${toCount}`;
+        newListItem.classList.add('filter__show-content-item');
+
+        let divEl = document.createElement('div');
+        divEl.classList.add('filter__show-content-item__btn-svg');
+        newListItem.appendChild(divEl);
+
+        filterShowContentList.appendChild(newListItem);
+        return newListItem;
+    };
+
+    const resetBtn = document.querySelector('.reset__btn');
+    resetBtn.addEventListener('click', (e) => {
+        let items = filterShowContentList.querySelectorAll('.filter__show-content-item');
+        items.forEach(item => {
+            item.remove();
+        });
+    })
 }
